73c8859a5666b3cee7fbe7a4b3c48a1f5724884d
Dim4IQ <dim4iq@gmail.com>
2020-06-28 15:22:49 +0300

    second step - hero is moving

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,31 @@ struct Tcod {
     root: Root,
 }
 
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        // Alt + Enter - fullscreen mode
+        Key { code: Enter, alt: true, .. } => {
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        },
+        // Esc - exit game
+        Key { code: Escape, .. } => return true,
+        //  movement keys
+        Key { code: Up, ..} => *player_y -= 1,
+        Key { code: Down, ..} => *player_y += 1,
+        Key { code: Left, ..} => *player_x -= 1,
+        Key { code: Right, ..} => *player_x += 1,
+
+        _ => {}
+    }
+    
+    false
+}
+
 fn main() {
     tcod::system::set_fps(LIMIT_FPS);
 
@@ -23,11 +48,18 @@ fn main() {
 
     let mut tcod = Tcod { root };
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !tcod.root.window_closed() {
         tcod.root.set_default_foreground(WHITE);
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
+        
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
     }
 }
